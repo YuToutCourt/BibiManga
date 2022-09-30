b55897edd31d327cfe3ddf828da36c311373094d
maleyy <wapzif@gmail.com>
2022-09-30 12:39:04 +0200

    Save db

--- dataBaseSave/manga_202209301238.sql
+++ dataBaseSave/manga_202209301238.sql
@@ -4,22 +4,22 @@ INSERT INTO bibimanga.manga (nom,start_date,end_date,status) VALUES
 	 ('Tokyo Ghoul:Re','2014-10-16','2018-07-05','Completed'),
 	 ('Death Note','2003-12-01','2006-06-15','Not finished'),
 	 ('Tokyo Revengers','2017-03-01',NULL,'Not finished'),
-	 ('The Quintessential Quintuplets','2017-08-09','2020-02-19','Not finished'),
+	 ('The Quintessential Quintuplets','2017-08-09','2020-02-19','Completed'),
 	 ('Dr. STONE','2017-03-06',NULL,'Not finished'),
 	 ('The Promised Neverland','2016-08-01','2020-06-15','Completed'),
 	 ('Mushoku Tensei','2014-05-02',NULL,'Not finished'),
 	 ('L''Attaque des Titans','2009-09-09','2021-04-09','Completed');
 INSERT INTO bibimanga.manga (nom,start_date,end_date,status) VALUES
-	 ('Darwin''s Game ','2012-12-12',NULL,'Not finished'),
-	 ('My Hero Academia ','2014-07-07',NULL,'Not finished'),
-	 ('Spy x Family ','2019-03-25',NULL,'Not finished'),
-	 ('Goblin Slayer ','2016-05-25',NULL,'Not finished'),
-	 ('Made in Abyss ','2012-10-20',NULL,'Not finished'),
-	 ('One-Punch Man ','2012-06-14',NULL,'Not finished'),
-	 ('Bleach ','2001-08-07','2016-08-23','Not finished'),
+	 ('Darwin''s Game','2012-12-12',NULL,'Not finished'),
+	 ('My Hero Academia','2014-07-07',NULL,'Not finished'),
+	 ('Spy x Family','2019-03-25',NULL,'Not finished'),
+	 ('Goblin Slayer','2016-05-25',NULL,'Not finished'),
+	 ('Made in Abyss','2012-10-20',NULL,'Not finished'),
+	 ('One-Punch Man','2012-06-14',NULL,'Not finished'),
+	 ('Bleach','2001-08-07','2016-08-23','Not finished'),
 	 ('Mashle','2020-01-26',NULL,'Not finished'),
-	 ('One Piece ','1997-07-22',NULL,'Not finished'),
-	 ('Seven Deadly Sins ','2012-10-10','2020-03-25','Not finished');
+	 ('One Piece','1997-07-22',NULL,'Not finished'),
+	 ('Seven Deadly Sins','2012-10-10','2020-03-25','Not finished');
 INSERT INTO bibimanga.manga (nom,start_date,end_date,status) VALUES
 	 ('Demon Slayer','2016-02-15','2020-05-18','Not finished'),
 	 ('Edens Zero','2018-06-26',NULL,'Not finished'),
@@ -29,10 +29,15 @@ INSERT INTO bibimanga.manga (nom,start_date,end_date,status) VALUES
 	 ('Your Name','2017-07-25','2019-03-26','Completed'),
 	 ('Parasyte','1989-10-22','1995-12-23','Not finished'),
 	 ('Atlantid','2014-06-25','2015-08-25','Completed'),
-	 ('To Your Eternity ','2016-11-09',NULL,'Not finished'),
+	 ('To Your Eternity','2016-11-09',NULL,'Not finished'),
 	 ('Je suis un assassin','2018-07-10',NULL,'Not finished');
 INSERT INTO bibimanga.manga (nom,start_date,end_date,status) VALUES
 	 ('Fire Force','2015-09-23',NULL,'Not finished'),
 	 ('Food Wars','2012-11-26','2019-06-17','Not finished'),
 	 ('Oshi no Ko','2020-08-23',NULL,'Not finished'),
-	 ('Toilet-Bound Hanako-kun','2014-06-18',NULL,'Not finished');
+	 ('Toilet-Bound Hanako-kun','2014-06-18',NULL,'Not finished'),
+	 ('Frieren','2020-04-28',NULL,'Not finished'),
+	 ('Violence Action','2016-04-25',NULL,'Not finished'),
+	 ('Colocataires à leur manière','2015-06-05',NULL,'Not finished'),
+	 ('Otaku Otaku','2014-04-17','2021-07-16','Not finished'),
+	 ('Moi, quand je me reincarne en slime','2015-03-26',NULL,'Not finished');
